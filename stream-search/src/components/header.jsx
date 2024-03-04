27944211b2f51f15ac840c0b4f2a83753d06b201
Amaan Omar <amaanomar3@gmail.com>
2024-03-04 17:18:33 +0200

    initial commit

---
+++ stream-search/src/components/header.jsx
@@ -0,0 +1,10 @@
+function header() {
+    return (
+        <div>
+            <div>
+                <h2>StreamSearch</h2>
+                <p>The best way to find all your favorite movies</p>
+            </div>
+        </div>
+    )
+}
